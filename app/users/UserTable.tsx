dd24c251c59c6ba5acb3041043196c8ab950d974
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-07-14 15:23:11 +0200

    examples using routes

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Link from "next/link";
+import { sort } from 'fast-sort';
 
 interface User {
   id: number;
@@ -6,18 +8,31 @@ interface User {
   email: string;
 }
 
-const UserTable = async () => {
+interface Props {
+  sortOrder?: string
+}
+
+const UserTable = async ({ sortOrder }: Props) => {
   const response = await fetch(
     'https://jsonplaceholder.typicode.com/users',
     { cache: 'no-store' });
-  const users: User[] = await response.json();
+  let users: User[] = await response.json();
+
+  switch (sortOrder) {
+    case "name":
+      users = sort(users).asc(u => u.name);
+      break;
+    case "email":
+      users = sort(users).asc(u => u.email);
+      break;
+  }
 
   return (
     <table className="table table-bordered">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Email</th>
+          <th><Link href="/users?sortOrder=name">Name</Link></th>
+          <th><Link href="/users?sortOrder=email">Email</Link></th>
         </tr>
       </thead>
       <tbody>
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { Suspense } from 'react';
+import Link from "next/link";
 import UserTable from './UserTable';
 
-const UsersPage = async () => {
+interface Props {
+  searchParams: { sortOrder?: string }
+}
+
+const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
 
   return (
     <>
       <h1>Users</h1>
-      <UserTable />
+      <Link href="/users/new" className="btn">New User</Link>
+      <Suspense fallback={<p>Loading...</p>}>
+        <UserTable sortOrder={sortOrder} />
+      </Suspense>
     </>
   );
 }
